cee0049900719d88be1f046c72a891810c4d77fc
Gordon Ball <gordon@chronitis.net>
2013-01-23 14:02:21 +0100

    AIs return objects rather than names

--- ai/stupid.py
+++ ai/stupid.py
@@ -3,24 +3,28 @@ import random
 import collections
 
 class StupidAI(AI):
+    """
+    StupidAI: Plays a completely random game, randomly choosing and reinforcing
+    territories, and attacking wherever it can without any considerations of wisdom.
+    """
     def initial_placement(self, empty, remaining):
         if empty:
             return random.choice(empty)
         else:
             t = random.choice(list(self.player.territories))
-            return t.name
+            return t
 
     def attack(self):
         for t in self.player.territories:
             for a in t.connect:
                 if a.owner != self.player:
                     if t.forces > a.forces:
-                        yield (t.name, a.name, None, None)
+                        yield (t, a, None, None)
 
     def reinforce(self, available):
         border = [t for t in self.player.territories if t.border]
         result = collections.defaultdict(int)
         for i in range(available):
-            t = random.choice(border).name
+            t = random.choice(border)
             result[t] += 1
         return result
